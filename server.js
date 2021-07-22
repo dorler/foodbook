fba2496d3a0110391d720451eee7b4b27b3aa635
Deb Orler <debbieorler@gmail.com>
2021-07-22 10:05:10 -0500

    variables defined

--- server.js
+++ server.js
@@ -1,1 +1,5 @@
-
+const path = require('path');
+const express = require('express');
+const sessions = require('express-sessions');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
