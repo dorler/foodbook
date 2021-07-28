e83262b8a23c10999aefcfec5943dae37ee9cea4
Deb Orler <debbieorler@gmail.com>
2021-07-28 13:15:12 -0500

    if I have to fix this all-post err one more time...

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -12,7 +12,7 @@ router.get('/', withAuth, async (req, res) => {
 
     const posts = postData.map((post) => post.get({ plain: true }));
 
-    res.render('all-posts', {
+    res.render('all-post', {
       layout: 'dashboard',
       posts,
     });
